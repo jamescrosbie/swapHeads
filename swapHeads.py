0f967d927a9fe78cf68ea47696a08ecdec69bfe6
jamescrosbie <jamescrosbie@hotmail.co.uk>
2019-08-27 14:06:39 +0100

    head images resized and imposed on good image

--- swapHeads.py
+++ swapHeads.py
@@ -4,6 +4,7 @@ import numpy
 # load images
 img1 = cv2.imread("./photos/goodHead.jpg", cv2.IMREAD_COLOR)
 img2 = cv2.imread("./photos/badHead.jpg", cv2.IMREAD_COLOR)
+good = img1.copy()
 bad = img2.copy()
 
 # using the haarcascades - frontal face default
@@ -29,14 +30,14 @@ for (x, y, w, h) in faces1:
     cv2.rectangle(img1, (x, y), (x + w, y + h), (255, 0, 0), 2)
     cv2.putText(img1, positionString, (x + w, y + h), font,
                 1, (255, 0, 0), 2, cv2.LINE_AA)
-goodROI = (735, 46, 94, 94)
+goodROI = (730, 46, 100, 100)
 
 for (x, y, w, h) in faces2:
     positionString = str(x) + ", " + str(y) + ", " + str(w) + ", " + str(h)
     cv2.rectangle(img2, (x, y), (x + w, y + h), (255, 0, 0), 2)
     cv2.putText(img2, positionString, (x + w, y + h), font,
                 1, (255, 0, 0), 2, cv2.LINE_AA)
-badROI = (729, 2, 94, 94)
+badROI = (715, 0, 120, 120)
 
 # show images
 cv2.imshow("goodHead", img1)
@@ -46,23 +47,32 @@ cv2.imshow("badHead", img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-
 # Off with their head !
-goodHead = img1[goodROI[1]: goodROI[1] + goodROI[2],
+goodHead = good[goodROI[1]: goodROI[1] + goodROI[2],
                 goodROI[0]: goodROI[0] + goodROI[3]]
-badHead = img2[badROI[1]: badROI[1] + badROI[2],
-               badROI[0]: badROI[0] + badROI[3]]
+badHead = bad[badROI[1]: badROI[1] + badROI[2],
+              badROI[0]: badROI[0] + badROI[3]]
 
 cv2.imshow("goodHead", goodHead)
 cv2.imshow("badHead", badHead)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# SwapHeads
-imgNew = bad
-imgNew[badROI[1]: badROI[1] + badROI[2],
-       badROI[0]: badROI[0] + badROI[3]] = goodHead
 
-cv2.imshow("New Head", imgNew)
+method = [cv2.INTER_AREA, cv2.INTER_NEAREST, cv2.INTER_LINEAR, cv2.INTER_AREA,
+          cv2.INTER_CUBIC, cv2.INTER_LANCZOS4]
+
+for m in method:
+    resizedGoodHead = cv2.resize(
+        goodHead, (badROI[2], badROI[3]), interpolation=m)
+
+    # SwapHeads
+    imgNew = bad
+
+    imgNew[badROI[1]: badROI[1] + badROI[2],
+           badROI[0]: badROI[0] + badROI[3]] = resizedGoodHead
+
+    cv2.imshow(f"New Head {m}", imgNew)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
